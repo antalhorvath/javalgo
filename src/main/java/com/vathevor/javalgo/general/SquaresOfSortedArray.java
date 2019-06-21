064390936150512e9b5903d200da46c4ad8a7277
antalhorvath <antal.horvath.90@gmail.com>
2019-06-21 20:21:38 +0200

    Define squares-of-sorted-array proble

---
+++ src/main/java/com/vathevor/javalgo/general/SquaresOfSortedArray.java
@@ -0,0 +1,12 @@
+package com.vathevor.javalgo.general;
+
+/**
+ * Given an array of integers sorted in non-decreasing order.
+ * Return an array of the squares of each number, also in sorted non-decreasing order.
+ */
+public class SquaresOfSortedArray {
+
+    static int[] sortedSquares(int[] elements) {
+        return null;
+    }
+}
