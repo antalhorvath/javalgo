e6d6ea0ee4e9043eae086097d34c6f0ae5d9e185
antalhorvath <antal.horvath.90@gmail.com>
2019-06-21 19:54:12 +0200

    Define fizz-buzz problem

---
+++ src/main/java/com/vathevor/javalgo/general/FizzBuzz.java
@@ -0,0 +1,14 @@
+package com.vathevor.javalgo.general;
+
+/**
+ * Write a program that concatenates the numbers from 1 to 'length' into a string.
+ * But for multiples of three concatenate “Fizz” instead of the number
+ * and for the multiples of five concatenate “Buzz”.
+ * For numbers which are multiples of both three and five print “FizzBuzz”.
+ */
+public class FizzBuzz {
+
+    static String getFizzBuzz() {
+        return "";  
+    }
+}
