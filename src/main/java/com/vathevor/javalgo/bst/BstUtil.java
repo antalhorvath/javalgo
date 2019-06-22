2752f6d4b84992f2f8586dc1a616a0ff2f9e5f0c
antalhorvath <antal.horvath.90@gmail.com>
2019-06-22 23:11:18 +0200

    Define bst-inordersuccessorpoblem

--- src/main/java/com/vathevor/javalgo/bst/BstUtil.java
+++ src/main/java/com/vathevor/javalgo/bst/BstUtil.java
@@ -10,6 +10,9 @@ public class BstUtil {
      * The left subtree of a node contains only nodes with value less than the node's value.
      * The right subtree of a node contains only nodes with greater than the node's value.
      * Bot the left and right subtree is Binary Search Trees.
+     *
+     * @param root the root node of the Binary Search Tree
+     * @return the result of the validity check
      */
     static boolean isValid(TreeNode<Integer> root) {
         return isValid(root, null, null);
@@ -23,5 +26,16 @@ public class BstUtil {
                 (high == null || node.getValue() < high) &&
                 isValid(node.getLeft(), low, node.getValue()) &&
                 isValid(node.getRight(), node.getValue(), high);
+    }
+
+    /**
+     * Determines the successor of h given node in the Binary Search Tree.
+     *
+     * @param root the root node of the Binary Search Tree
+     * @param node the node whose successor should be returned
+     * @return the successor of given node
+     */
+    static TreeNode<Integer> getInorderSuccessor(TreeNode<Integer> root, TreeNode<Integer> node) {
+        return null;
     }
 }
--- src/test/java/com/vathevor/javalgo/bst/BstUtilTest.java
+++ src/test/java/com/vathevor/javalgo/bst/BstUtilTest.java
@@ -3,6 +3,7 @@ package com.vathevor.javalgo.bst;
 import com.vathevor.javalgo.bst.model.TreeNode;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,17 +11,21 @@ class BstUtilTest {
 
     @Test
     void shouldBeValid() {
+        TreeNode<Integer> root = createValidBinarySearchTree();
+
+        boolean valid = BstUtil.isValid(root);
+
+        assertTrue(valid);
+    }
+
+    private TreeNode<Integer> createValidBinarySearchTree() {
         TreeNode<Integer> leftOfLeft = new TreeNode<>(1);
         TreeNode<Integer> rightOfLeft = new TreeNode<>(3);
         TreeNode<Integer> left = new TreeNode<>(2, leftOfLeft, rightOfLeft);
         TreeNode<Integer> leftOfRight = new TreeNode<>(5);
         TreeNode<Integer> rightOfRight = new TreeNode<>(7);
         TreeNode<Integer> right = new TreeNode<>(6, leftOfRight, rightOfRight);
-        TreeNode<Integer> root = new TreeNode<>(4, left, right);
-
-        boolean valid = BstUtil.isValid(root);
-
-        assertTrue(valid);
+        return new TreeNode<>(4, left, right);
     }
 
     @Test
@@ -34,5 +39,15 @@ class BstUtilTest {
         boolean valid = BstUtil.isValid(root);
 
         assertFalse(valid);
+    }
+
+    @Test
+    void shouldReturnTheNodeWithValueTwo() {
+        TreeNode<Integer> root = createValidBinarySearchTree();
+        TreeNode<Integer> node = new TreeNode<>(1);
+
+        TreeNode<Integer> inorderSuccessor = BstUtil.getInorderSuccessor(root, node);
+
+        assertEquals(2, inorderSuccessor.getValue());
     }
 }
