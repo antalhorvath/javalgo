041c7a7778191fccfd5f6eadc395b2c9b7b1d41c
antalhorvath <antal.horvath.90@gmail.com>
2019-06-22 22:15:55 +0200

    Define bst-validator problem

---
+++ src/main/java/com/vathevor/javalgo/bst/BstValidator.java
@@ -0,0 +1,17 @@
+package com.vathevor.javalgo.bst;
+
+import com.vathevor.javalgo.bst.model.TreeNode;
+
+/**
+ * Determine if the given Binary Search Tree is valid.
+ * It is valid if:
+ * The left subtree of a node contains only nodes with value less than the node's value.
+ * The right subtree of a node contains only nodes with greater than the node's value.
+ * Bot the left and right subtree is Binary Search Trees.
+ */
+public class BstValidator {
+
+    static boolean isValid(TreeNode<Integer> root) {
+        return false;
+    }
+}
---
+++ src/main/java/com/vathevor/javalgo/bst/model/TreeNode.java
@@ -0,0 +1,30 @@
+package com.vathevor.javalgo.bst.model;
+
+public class TreeNode<T> {
+
+    private T value;
+    private TreeNode<T> left;
+    private TreeNode<T> right;
+
+    public TreeNode(T value) {
+        this.value = value;
+    }
+
+    public TreeNode(T value, TreeNode<T> left, TreeNode<T> right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public TreeNode<T> getLeft() {
+        return left;
+    }
+
+    public TreeNode<T> getRight() {
+        return right;
+    }
+}
