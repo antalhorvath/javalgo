9024bee898c0c7fb03d9be94ab0d863fae86cb11
antalhorvath <antal.horvath.90@gmail.com>
2019-06-23 20:35:15 +0200

    Implement iterator

--- src/main/java/com/vathevor/javalgo/bst/BstIterator.java
+++ src/main/java/com/vathevor/javalgo/bst/BstIterator.java
@@ -2,23 +2,43 @@ package com.vathevor.javalgo.bst;
 
 import com.vathevor.javalgo.bst.model.TreeNode;
 
+import java.util.Stack;
+
+/*
+ * Implement an iterator over a binary search tree (BST).
+ * Your iterator will be initialized with the root node of a BST.
+ * next() and hasNext() should run in average O(1) time
+ * and uses O(h) memory, where h is the height of the tree.
+ */
 public class BstIterator {
 
-    public BstIterator(TreeNode root) {
+    private Stack<TreeNode> stack;
 
+    public BstIterator(TreeNode root) {
+        stack = new Stack<>();
+        pushAllNodesOf(root);
     }
 
     /**
      * @return the next smallest number
      */
     public int next() {
-        return 0;
+        TreeNode currentNode = stack.pop();
+        pushAllNodesOf(currentNode.getRight());
+        return currentNode.getValue();
     }
 
     /**
      * @return whether we have a next smallest number
      */
     public boolean hasNext() {
-        return false;
+        return !stack.isEmpty();
+    }
+
+    private void pushAllNodesOf(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.getLeft();
+        }
     }
 }
