62bc7800825e1432822c4c617b4d041d88aa5e7c
antalhorvath <antal.horvath.90@gmail.com>
2019-06-24 23:08:55 +0200

    Define hourglass problem

---
+++ src/main/java/com/vathevor/javalgo/array/HourGlassSum.java
@@ -0,0 +1,19 @@
+package com.vathevor.javalgo.array;
+
+/*
+ * Given a 6x6 2D array.
+ * An hourglass looks like the following
+ *
+ * a b c
+ *   d
+ * e f g
+ *
+ * There are 16 hourglasses in the given array.
+ * Calculate the hourglass sum for every hourglass in the array and return the maximum value.
+ */
+public class HourGlassSum {
+
+    static int hourglassSum(int[][] arr) {
+        return 42;
+    }
+}
---
+++ src/test/java/com/vathevor/javalgo/array/HourGlassSumTest.java
@@ -0,0 +1,62 @@
+package com.vathevor.javalgo.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HourGlassSumTest {
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    void testHourglassSum(TestCase testCase) {
+        int output = HourGlassSum.hourglassSum(testCase.inputGrid);
+
+        assertEquals(testCase.expectedOutput, output);
+    }
+
+    private static class TestCase {
+        int[][] inputGrid;
+        int expectedOutput;
+    }
+
+    static Stream<TestCase> testCases() {
+        TestCase testCaseA = new TestCase();
+        testCaseA.inputGrid = new int[][]{
+                {1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0}
+        };
+        testCaseA.expectedOutput = 7;
+
+        TestCase testCaseB = new TestCase();
+        testCaseB.inputGrid = new int[][]{
+                {-9, -9, -9, 1, 1, 1},
+                {0, -9, 0, 4, 3, 2},
+                {-9, -9, -9, 1, 2, 3},
+                {0, 0, 8, 6, 6, 0},
+                {0, 0, 0, -2, 0, 0},
+                {0, 0, 1, 2, 4, 0}
+        };
+        testCaseB.expectedOutput = 28;
+
+        TestCase testCaseC = new TestCase();
+        testCaseC.inputGrid = new int[][]{
+                {1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0},
+                {0, 0, 2, 4, 4, 0},
+                {0, 0, 0, 2, 0, 0},
+                {0, 0, 1, 2, 4, 0}
+        };
+        testCaseC.expectedOutput = 19;
+
+        return Stream.of(testCaseA, testCaseB, testCaseC);
+    }
+}
