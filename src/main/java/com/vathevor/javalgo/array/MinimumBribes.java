de5d7c9f18dcac7a2d4463214f301906f3c9d260
antalhorvath <antal.horvath.90@gmail.com>
2019-06-25 22:38:48 +0200

    Solve problem

--- src/main/java/com/vathevor/javalgo/array/MinimumBribes.java
+++ src/main/java/com/vathevor/javalgo/array/MinimumBribes.java
@@ -14,7 +14,19 @@ package com.vathevor.javalgo.array;
 public class MinimumBribes {
 
     static String getMinimumBribes(int[] q) {
-        return "";
+        int minimumBribes = 0;
+        for (int i = 0; i < q.length; i++) {
+            int shift = q[i] - (i + 1);
+            if (2 < shift) {
+                return "Too chaotic";
+            }
+            for (int j = Math.max(0, q[i] - 2); j < i; j++) {
+                if (q[i] < q[j]) {
+                    minimumBribes++;
+                }
+            }
+        }
+        return String.valueOf(minimumBribes);
     }
 }
 
