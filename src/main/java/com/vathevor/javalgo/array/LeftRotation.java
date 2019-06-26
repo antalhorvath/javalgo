465c5558930c3f437ddeb510bf2542bbd1265bb7
antalhorvath <antal.horvath.90@gmail.com>
2019-06-26 21:29:36 +0200

    Define left-rotation problem

---
+++ src/main/java/com/vathevor/javalgo/array/LeftRotation.java
@@ -0,0 +1,22 @@
+package com.vathevor.javalgo.array;
+
+/*
+ * A left rotation operation on an array shifts each of the array's elements unit to the left.
+ * For example, if left rotations are performed on array [1,2,3,4,5],
+ * then the array would become [3,4,5,2,1].
+ *
+ * Complete the function rotLeft in the editor below.
+ * It should return the resulting array of integers.
+ */
+public class LeftRotation {
+
+    /**
+     *
+     * @param a array of integers
+     * @param d d number of rotations
+     * @return
+     */
+    static int[] rotLeft(int[] a, int d) {
+        return new int[0];
+    }
+}
