168e7260ce94324e803a2c5704202f8954af84fc
antalhorvath <antal.horvath.90@gmail.com>
2019-07-01 21:37:48 +0200

    Define problem

---
+++ src/main/java/com/vathevor/javalgo/array/ValidMountain.java
@@ -0,0 +1,16 @@
+package com.vathevor.javalgo.array;
+
+/*
+ * Given an array A of integers, return true if and only if it is a valid mountain array.
+ * Recall that A is a mountain array if and only if:
+ *  - A.length >= 3
+ *  - There exists some i with 0 < i < A.length - 1 such that:
+ *      - A[0] < A[1] < ... A[i-1] < A[i]
+ *      - A[i] > A[i+1] > ... > A[A.length - 1]
+ */
+public class ValidMountain {
+
+    public boolean validMountainArray(int[] A) {
+        return false;
+    }
+}
