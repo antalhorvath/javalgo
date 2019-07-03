b6faa929cc91e268eedff8ce30fefae9249fb45a
antalhorvath <antal.horvath.90@gmail.com>
2019-07-03 21:57:48 +0200

    Define merge-intervals problem

---
+++ src/main/java/com/vathevor/javalgo/sort/MergeIntervals.java
@@ -0,0 +1,16 @@
+package com.vathevor.javalgo.sort;
+
+/*
+ * Given a collection of intervals, merge all overlapping intervals.
+ *
+ * Example 1:
+ *      Input: [[1,3],[2,6],[8,10],[15,18]]
+ *      Output: [[1,6],[8,10],[15,18]]
+ *      Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ */
+public class MergeIntervals {
+
+    public int[][] merge(int[][] intervals) {
+        return null;
+    }
+}
