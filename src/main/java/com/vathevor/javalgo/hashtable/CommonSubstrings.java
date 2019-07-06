3ce07e27b257a7c823c0cd3f05355f4457f4eaa2
antalhorvath <antal.horvath.90@gmail.com>
2019-07-06 11:22:34 +0200

    Define common substrings problem

---
+++ src/main/java/com/vathevor/javalgo/hashtable/CommonSubstrings.java
@@ -0,0 +1,19 @@
+package com.vathevor.javalgo.hashtable;
+
+/*
+ * Given two strings, determine if they share a common substring.
+ * A substring may be as small as one character.
+ *
+ * For example, the words "a", "and", "art" share the common substring
+ *      The words "be" and "cat" do not share a substring.
+ *
+ * Function Description
+ *      Complete the function twoStrings in the editor below.
+ *      It should return a string, either YES or NO based on whether the strings share a common substring.
+ */
+public class CommonSubstrings {
+
+    static String twoStrings(String s1, String s2) {
+        return "";
+    }
+}
