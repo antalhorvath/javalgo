954aac5b17893429833a44f03125be7d93f4bc1b
antalhorvath <antal.horvath.90@gmail.com>
2019-07-06 11:13:52 +0200

    Solve problem

--- src/main/java/com/vathevor/javalgo/general/NextClosestTime.java
+++ src/main/java/com/vathevor/javalgo/general/NextClosestTime.java
@@ -1,5 +1,10 @@
 package com.vathevor.javalgo.general;
 
+import java.util.Arrays;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+
 /*
  * Given a time represented in the format "HH:MM",
  * form the next closest time by reusing the current digits.
@@ -24,7 +29,37 @@ package com.vathevor.javalgo.general;
  */
 public class NextClosestTime {
 
+    private static final int MINUTES_OF_HOUR = 60;
+    private static final int MINUTES_OF_DAY = 24 * MINUTES_OF_HOUR;
+
+    private static int[] getDigits(int timeInMinutes) {
+        int hours = timeInMinutes / 60;
+        int minutes = timeInMinutes % 60;
+        return new int[]{hours / 10, hours % 10, minutes / 10, minutes % 10};
+    }
+
+    private static boolean consistsOfAllowedDigits(int number, Set<Integer> allowedDigits) {
+        int[] digits = getDigits(number);
+        for (int digit : digits) {
+            if (!allowedDigits.contains(digit)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public String nextClosestTime(String time) {
-        return "";
+        int hours = Integer.valueOf(time.substring(0, 2));
+        int minutes = Integer.valueOf(time.substring(3));
+        int now = hours * MINUTES_OF_HOUR + minutes;
+        Set<Integer> allowedDigits = Arrays.stream(getDigits(now)).boxed().collect(toSet());
+
+        int closestTime = now;
+        while (true) {
+            closestTime = (closestTime + 1) % MINUTES_OF_DAY;
+            if (consistsOfAllowedDigits(closestTime, allowedDigits)) {
+                return String.format("%02d:%02d", closestTime / MINUTES_OF_HOUR, closestTime % MINUTES_OF_HOUR);
+            }
+        }
     }
 }
