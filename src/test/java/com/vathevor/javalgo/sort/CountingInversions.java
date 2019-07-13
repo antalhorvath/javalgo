369298378bb8d42c1c1e8dfed6ff0166c8f31790
antalhorvath <antal.horvath.90@gmail.com>
2019-07-13 12:55:57 +0200

    Solve problem

--- src/test/java/com/vathevor/javalgo/sort/CountingInversions.java
+++ src/test/java/com/vathevor/javalgo/sort/CountingInversions.java
@@ -12,10 +12,52 @@ package com.vathevor.javalgo.sort;
 public class CountingInversions {
 
     /**
-     * @param arr  an array of integers to sort
+     * @param arr an array of integers to sort
      * @return an integer representing the number of inversions required to sort the array
      */
     static long countInversions(int[] arr) {
-        return 0;
+        int[] temp = new int[arr.length];
+        return mergeSort(arr, temp, 0, arr.length - 1);
+    }
+
+    static Long mergeSort(int[] arr, int[] temp, int start, int end) {
+        long count = 0;
+        if (start < end) {
+            int mid = (start + end) / 2;
+            count = mergeSort(arr, temp, start, mid);
+            count += mergeSort(arr, temp, mid + 1, end);
+            count += merge(arr, temp, start, mid + 1, end);
+        }
+        return count;
+    }
+
+    static int merge(int[] arr, int[] temp, int start, int middle, int end) {
+        int count = 0;
+        int i = start;
+        int j = middle;
+        int k = start;
+
+        while ((i <= middle - 1) && (j <= end)) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                count = count + (middle - i);
+            }
+        }
+
+        while (i <= middle - 1) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = start; i <= end; i++) {
+            arr[i] = temp[i];
+        }
+
+        return count;
     }
 }
