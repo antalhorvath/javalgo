38d660facd413f7e4c3f27556e5984604b694719
antalhorvath <antal.horvath.90@gmail.com>
2019-07-13 17:26:26 +0200

    Solve problem

--- src/main/java/com/vathevor/javalgo/general/ZigZagConversion.java
+++ src/main/java/com/vathevor/javalgo/general/ZigZagConversion.java
@@ -19,6 +19,36 @@ package com.vathevor.javalgo.general;
 public class ZigZagConversion {
 
     public static String convert(String s, int numRows) {
-        return "";
+        if(s.length() < 2) {
+            return s;
+        }
+
+        int[] indices = new int[s.length()];
+        int p = 0;
+
+        int l = numRows + numRows - 2;
+        int k = s.length() / l + 1;
+        for (int j = 0; j < numRows; j++) {
+            for (int i = 0; i < k; i++) {
+                int v = i * l + j;
+                if (v < s.length()) {
+                    indices[p++] = v;
+                    int remainder = v % l;
+                    if (remainder != 0) {
+                        int w = (v / l + 1) * l - remainder;
+                        if (w < s.length() && v != w) {
+                            indices[p++] = w;
+                        }
+                    }
+                }
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            result.append(s.charAt(indices[i]));
+        }
+
+        return result.toString();
     }
 }
