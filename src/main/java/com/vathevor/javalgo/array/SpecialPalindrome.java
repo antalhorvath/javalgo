adfe7e5f93786e0ee39ab05d6b37daef030aba78
antalhorvath <antal.horvath.90@gmail.com>
2019-07-14 19:58:13 +0200

    Define special palindrome problem

---
+++ src/main/java/com/vathevor/javalgo/array/SpecialPalindrome.java
@@ -0,0 +1,26 @@
+package com.vathevor.javalgo.array;
+
+/*
+ * A string is said to be a special string if either of two conditions is met:
+ *     All of the characters are the same, e.g. aaa.
+ *     All characters except the middle one are the same, e.g. aadaa.
+ *
+ * A special substring is any substring of a string which meets one of those criteria. Given a string,
+ * determine how many special substrings can be formed from it.
+ *
+ * For example, given the string s = mnonopoo, we have the following special substrings:
+ * {m, n, o, n, o, p, o, o, non, ono, opo, oo}
+ *
+ */
+public class SpecialPalindrome {
+
+    /**
+     *
+     * @param n length of s
+     * @param s string
+     * @return an integer representing the number of special substrings that can be formed from the given string
+     */
+    static long substrCount(int n, String s) {
+        return 0;
+    }
+}
