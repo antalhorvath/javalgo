746fc3c543b8bff683f8fad73d3f35fc3fe5d790
antalhorvath <antal.horvath.90@gmail.com>
2019-07-28 21:26:15 +0200

    Solve problem

--- src/main/java/com/vathevor/javalgo/general/RomanNumber.java
+++ src/main/java/com/vathevor/javalgo/general/RomanNumber.java
@@ -2,9 +2,13 @@ package com.vathevor.javalgo.general;
 
 public class RomanNumber {
 
+    private static final String[] SYMBOLS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static final int[] VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
     private String value;
 
-    private RomanNumber() {
+    private RomanNumber(String value) {
+        this.value = value;
     }
 
     public String getValue() {
@@ -12,6 +16,13 @@ public class RomanNumber {
     }
 
     public static RomanNumber valueOf(Integer integer) {
-        return new RomanNumber();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < VALUES.length; i++) {
+            while (integer >= VALUES[i]) {
+                integer -= VALUES[i];
+                result.append(SYMBOLS[i]);
+            }
+        }
+        return new RomanNumber(result.toString());
     }
 }
