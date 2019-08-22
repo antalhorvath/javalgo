84adc88ea4ee2ca6e9f63f9756ff20303d7fcd91
antalhorvath <antal.horvath.90@gmail.com>
2019-08-22 16:45:53 +0200

    Solve problem

--- src/main/java/com/vathevor/javalgo/search/DifferencePairs.java
+++ src/main/java/com/vathevor/javalgo/search/DifferencePairs.java
@@ -1,5 +1,10 @@
 package com.vathevor.javalgo.search;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /* You will be given an array of integers and a target value.
  * Determine the number of pairs of array elements that have a difference equal to a target value.
  * For example, given an array of [1, 2, 3, 4] and a target value of 1,
@@ -8,6 +13,17 @@ package com.vathevor.javalgo.search;
 public class DifferencePairs {
 
     static int pairs(int k, int[] arr) {
-        return 0;
+        Map<Integer, Long> counts = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        int result = 0;
+        for (int integer : arr) {
+            int subtrahend = integer - k;
+            Long countOfPossibleSubtrahends = counts.getOrDefault(subtrahend, 0L);
+            result += countOfPossibleSubtrahends;
+        }
+
+        return result;
     }
 }
